3f5795065f9b30104f308c1897542f7909e166fe
adamtopaz <github@adamtopaz.com>
2023-06-20 10:24:00 -0600

    skeleton for equiv

--- ExtrDisc/Basic.lean
+++ ExtrDisc/Basic.lean
@@ -8,7 +8,7 @@ open CategoryTheory
 
 structure ExtrDisc where
   compHaus : CompHaus.{u}
-  projective : Projective compHaus
+  [projective : Projective compHaus]
 
 namespace ExtrDisc
 
@@ -29,11 +29,43 @@ def toProfinite : ExtrDisc.{u} ⥤ Profinite.{u} where
   obj X := { toCompHaus := X.compHaus }
   map f := f
 
+instance : Full toProfinite := sorry
+instance : Faithful toProfinite := sorry
+
 @[simps!]
 def toCompHaus : ExtrDisc.{u} ⥤ CompHaus.{u} :=
   inducedFunctor _
 
+instance : Full toCompHaus := sorry
+instance : Faithful toCompHaus := sorry
+
 example : toProfinite ⋙ profiniteToCompHaus = toCompHaus := 
   rfl
 
+instance (X : ExtrDisc) : Projective X.compHaus := X.projective
+
 end ExtrDisc
+
+namespace CompHaus
+
+noncomputable
+def presentation (X : CompHaus) : ExtrDisc where
+  compHaus := (projectivePresentation X).p
+
+noncomputable
+def presentationπ  (X : CompHaus) : X.presentation.compHaus ⟶ X :=   
+  (projectivePresentation X).f
+
+noncomputable
+instance epiPresentπ (X : CompHaus) : Epi X.presentationπ :=   
+  (projectivePresentation X).epi
+
+noncomputable
+def lift {X Y : CompHaus} (f : X ⟶ Y) [Epi f] : Y.presentation.compHaus ⟶ X :=
+  Projective.factorThru Y.presentationπ f 
+
+@[simp, reassoc]
+lemma lift_lifts {X Y : CompHaus} (f : X ⟶ Y) [Epi f] :
+    lift f ≫ f  = Y.presentationπ := by simp [lift]
+
+end CompHaus
