e0c2dc82c81bfbdf0eab47b81f2fe50ada118060
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-06-20 21:48:54 +0100

    add docs for equivalence

--- Condensed/Equivalence.lean
+++ Condensed/Equivalence.lean
@@ -1,8 +1,34 @@
+/-
+Copyright (c) 2023 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
 import Profinite.Coherent
 import ExtrDisc.Coherent
 import Mathlib.Condensed.Basic
 import Mathlib.CategoryTheory.Sites.DenseSubsite
+/-!
+# Sheaves on CompHaus are equivalent to sheaves on ExtrDisc
 
+The forgetful functor from extremally disconnected spaces `ExtrDisc` to compact
+Hausdorff spaces `CompHaus` has the marvellous property that it induces an equivalence of categories
+between sheaves on these two sites. With the terminology of nLab, `ExtrDisc` is a
+*dense subsite* of `CompHaus`: see https://ncatlab.org/nlab/show/dense+sub-site
+
+Mathlib has isolated three properties called `CoverDense`, `CoverPreserving` and `CoverLifting`,
+which between them imply that `ExtrDisc` is a dense subsite, and it also has the
+construction of the equivalence of the categories of sheaves, given these three properties.
+
+## Main theorems
+
+* `Condensed.coverDense`, `Condensed.coverPreserving`, `Condensed.coverLifting`: the
+three conditions needed to guarantee the equivalence of the categories of sheaves
+on the two sites.
+
+## TODO
+
+Prove the three main theorems!
+-/
 open CategoryTheory Limits
 
 namespace Condensed
@@ -13,14 +39,14 @@ universe u w
 
 namespace ExtrDiscCompHaus
 
-def coverDense : CoverDense (coherentTopology _) ExtrDisc.toCompHaus := 
+theorem coverDense : CoverDense (coherentTopology _) ExtrDisc.toCompHaus := 
   sorry
     
-def coverPreserving : 
+theorem coverPreserving : 
     CoverPreserving (coherentTopology _) (coherentTopology _) ExtrDisc.toCompHaus := 
   sorry
 
-def coverLifting : 
+theorem coverLifting : 
     CoverLifting (coherentTopology _) (coherentTopology _) ExtrDisc.toCompHaus := 
   sorry
 
