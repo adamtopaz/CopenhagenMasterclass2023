73ae0b73c591a57a22524f095bdce02216c10473
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2023-06-20 21:32:57 +0100

    add docs for Profinite.Coherent

--- Profinite/Coherent.lean
+++ Profinite/Coherent.lean
@@ -1,6 +1,20 @@
+/-
+Copyright (c) 2023 Adam Topaz. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Adam Topaz
+-/
 import Mathlib.CategoryTheory.Sites.Coherent
 import Mathlib.Topology.Category.Profinite.Basic
+/-!
 
+# The category of profinite spaces is precoherent
+
+# TODO
+
+Prove the theorem that the category of profinite spaces
+is precoherent.
+
+-/
 namespace Profinite
 
 open CategoryTheory
