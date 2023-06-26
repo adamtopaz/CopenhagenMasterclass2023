636243426a794162bd0164e3a6183c933e7a72db
Jon Eugster <eugster.jon@gmail.com>
2023-06-26 14:15:12 +0200

    tfae 2 to 3

--- Profinite/Epi.lean
+++ Profinite/Epi.lean
@@ -1,25 +1,175 @@
 import Mathlib.Topology.Category.Profinite.Basic
 import Mathlib.Topology.Category.CompHaus.EffectiveEpi
 
-open CategoryTheory
+open CategoryTheory Limits
 
 namespace Profinite
 
 universe u
 
+section FiniteCoproducts
+
+variable {α : Type} [Fintype α] (X : α → Profinite.{u})
+
+/--
+The coproduct of a finite family of objects in `Profinite`, constructed as the disjoint
+union with its usual topology.
+-/
+def finiteCoproduct : Profinite := Profinite.of <| Σ (a : α), X a
+
+/--
+The inclusion of one of the factors into the explicit finite coproduct.
+-/
+def finiteCoproduct.ι (a : α) : X a ⟶ finiteCoproduct X where
+  toFun := fun x => ⟨a,x⟩
+  continuous_toFun := continuous_sigmaMk (σ := fun a => X a)
+
+/--
+To construct a morphism from the explicit finite coproduct, it suffices to
+specify a morphism from each of its factors.
+This is essentially the universal property of the coproduct.
+-/
+def finiteCoproduct.desc {B : Profinite.{u}} (e : (a : α) → (X a ⟶ B)) :
+    finiteCoproduct X ⟶ B where
+  toFun := fun ⟨a,x⟩ => e a x
+  continuous_toFun := by
+    apply continuous_sigma
+    intro a ; exact (e a).continuous
+
+@[reassoc (attr := simp)]
+lemma finiteCoproduct.ι_desc {B : Profinite.{u}} (e : (a : α) → (X a ⟶ B)) (a : α) :
+  finiteCoproduct.ι X a ≫ finiteCoproduct.desc X e = e a := rfl
+
+lemma finiteCoproduct.hom_ext {B : Profinite.{u}} (f g : finiteCoproduct X ⟶ B)
+    (h : ∀ a : α, finiteCoproduct.ι X a ≫ f = finiteCoproduct.ι X a ≫ g) : f = g := by
+  ext ⟨a,x⟩
+  specialize h a
+  apply_fun (fun q => q x) at h
+  exact h
+
+/--
+The coproduct cocone associated to the explicit finite coproduct.
+-/
+@[simps]
+def finiteCoproduct.cocone : Limits.Cocone (Discrete.functor X) where
+  pt := finiteCoproduct X
+  ι := Discrete.natTrans fun ⟨a⟩ => finiteCoproduct.ι X a
+
+/--
+The explicit finite coproduct cocone is a colimit cocone.
+-/
+@[simps]
+def finiteCoproduct.isColimit : Limits.IsColimit (finiteCoproduct.cocone X) where
+  desc := fun s => finiteCoproduct.desc _ fun a => s.ι.app ⟨a⟩
+  fac := fun s ⟨a⟩ => finiteCoproduct.ι_desc _ _ _
+  uniq := fun s m hm => finiteCoproduct.hom_ext _ _ _ fun a => by
+    specialize hm ⟨a⟩
+    ext t
+    apply_fun (fun q => q t) at hm
+    exact hm
+
+
+end FiniteCoproducts
+
+#check CategoryTheory.epiCoproductDescOfEffectiveEpiFamily
+
 open List in
-theorem effectiveEpiFamily_tfae {α : Type} [Fintype α] {B : Profinite.{u}} 
+theorem effectiveEpiFamily_tfae {α : Type} [Fintype α] {B : Profinite.{u}}
     (X : α → Profinite.{u})
     (π : (a : α) → (X a ⟶ B)) :
     TFAE [
       EffectiveEpiFamily X π,
       Epi (Limits.Sigma.desc π),
-      ∀ (b : B), ∃ (a : α) (x : X a), π a x = b 
+      ∀ (b : B), ∃ (a : α) (x : X a), π a x = b
     ] := by
-  tfae_have 1 → 2 
-  · intro ; infer_instance 
+  tfae_have 1 → 2
+  · intro
+    infer_instance
+    -- #check CategoryTheory.epiCoproductDescOfEffectiveEpiFamily
   tfae_have 2 → 3
-  · sorry 
+  · intro e
+    rw [epi_iff_surjective] at e
+    let i : ∐ X ≅ finiteCoproduct X :=
+      (colimit.isColimit _).coconePointUniqueUpToIso (finiteCoproduct.isColimit _)
+    intro b
+    obtain ⟨t,rfl⟩ := e b
+    let q := i.hom t
+    refine ⟨q.1,q.2,?_⟩
+    have : t = i.inv (i.hom t) := show t = (i.hom ≫ i.inv) t by simp only [i.hom_inv_id] ; rfl
+    rw [this]
+    show _ = (i.inv ≫ Sigma.desc π) (i.hom t)
+    suffices i.inv ≫ Sigma.desc π = finiteCoproduct.desc X π by
+      rw [this] ; rfl
+    rw [Iso.inv_comp_eq]
+    apply colimit.hom_ext
+    rintro ⟨a⟩
+    simp only [Discrete.functor_obj, colimit.ι_desc, Cofan.mk_pt, Cofan.mk_ι_app,
+      colimit.comp_coconePointUniqueUpToIso_hom_assoc]
+    ext ; rfl
   tfae_have 3 → 1
-  · sorry
+  · sorry -- apply effectiveEpiFamily_of_jointly_surjective
   tfae_finish
+
+end Profinite
+
+open CategoryTheory Limits
+
+
+/-! Copy from Comphaus -/
+namespace CompHaus
+
+namespace EffectiveEpiFamily
+
+variable {α : Type} [Fintype α] {B : CompHaus.{u}}
+  {X : α → CompHaus.{u}} (π : (a : α) → (X a ⟶ B))
+  (surj : ∀ b : B, ∃ (a : α) (x : X a), π a x = b)
+
+open List in
+theorem effectiveEpiFamily_tfae'
+    {α : Type} [Fintype α] {B : CompHaus.{u}}
+    (X : α → CompHaus.{u}) (π : (a : α) → (X a ⟶ B)) :
+    TFAE
+    [ EffectiveEpiFamily X π
+    , Epi (Sigma.desc π)
+    , ∀ b : B, ∃ (a : α) (x : X a), π a x = b
+    ] := by
+  tfae_have 1 → 2
+  · intro ; infer_instance
+  tfae_have 2 → 3
+  · intro e ; rw [epi_iff_surjective] at e
+    let i : ∐ X ≅ finiteCoproduct X :=
+      (colimit.isColimit _).coconePointUniqueUpToIso (finiteCoproduct.isColimit _)
+    intro b
+    obtain ⟨t,rfl⟩ := e b
+    let q := i.hom t
+    refine ⟨q.1,q.2,?_⟩
+    have : t = i.inv (i.hom t) := show t = (i.hom ≫ i.inv) t by simp only [i.hom_inv_id] ; rfl
+    rw [this]
+    show _ = (i.inv ≫ Sigma.desc π) (i.hom t)
+    suffices i.inv ≫ Sigma.desc π = finiteCoproduct.desc X π by
+      rw [this] ; rfl
+    rw [Iso.inv_comp_eq]
+    apply colimit.hom_ext
+    rintro ⟨a⟩
+    simp only [Discrete.functor_obj, colimit.ι_desc, Cofan.mk_pt, Cofan.mk_ι_app,
+      colimit.comp_coconePointUniqueUpToIso_hom_assoc]
+    ext ; rfl
+  tfae_have 3 → 1
+  · apply effectiveEpiFamily_of_jointly_surjective
+  tfae_finish
+
+instance precoherent : Precoherent CompHaus.{u} := by
+  constructor
+  intro B₁ B₂ f α _ X₁ π₁ h₁
+  refine ⟨α, inferInstance, fun a => pullback f (π₁ a), fun a => pullback.fst _ _, ?_,
+    id, fun a => pullback.snd _ _, ?_⟩
+  · have := (effectiveEpiFamily_tfae _ π₁).out 0 2 ; rw [this] at h₁ ; clear this
+    have := (effectiveEpiFamily_tfae _ (fun a => pullback.fst f (π₁ a))).out 0 2
+    rw [this] ; clear this
+    intro b₂
+    obtain ⟨a,x,h⟩ := h₁ (f b₂)
+    refine ⟨a, ⟨⟨b₂, x⟩, h.symm⟩, rfl⟩
+  · intro a
+    dsimp
+    ext ⟨⟨_,_⟩,h⟩
+    exact h.symm
