501de7d167d2c4ce350511ba26962af62c9bbe11
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-26 17:18:48 +0200

    progress

--- ExtrDisc/Coherent.lean
+++ ExtrDisc/Coherent.lean
@@ -29,13 +29,13 @@ def Family.toCompHaus {α : Type} (X : α → ExtrDisc.{u}) : α → CompHaus :=
   fun a => toCompHaus.obj (X a) 
 
 def Family.toCompHaus' {α : Type} {B : ExtrDisc.{u}} 
-    (X : α → ExtrDisc.{u})
+    {X : α → ExtrDisc.{u}}
     (π : (a : α) → (X a ⟶ B)) : (a : α) → (Family.toCompHaus X) a ⟶ ExtrDisc.toCompHaus.obj B :=
   fun a => ExtrDisc.toCompHaus.map (π a)
 
-theorem effectiveEpiFamily.toCompHaus {α : Type} [Fintype α] {B : ExtrDisc.{u}} (X : α → ExtrDisc.{u})
-    (π : (a : α) → (X a ⟶ B)) (H : EffectiveEpiFamily X π) :
-    EffectiveEpiFamily (Family.toCompHaus X) (Family.toCompHaus' X π) := by 
+theorem effectiveEpiFamily.toCompHaus {α : Type} [Fintype α] {B : ExtrDisc.{u}} {X : α → ExtrDisc.{u}}
+    {π : (a : α) → (X a ⟶ B)} (H : EffectiveEpiFamily X π) :
+    EffectiveEpiFamily (Family.toCompHaus X) (Family.toCompHaus' π) := by 
   refine' ((CompHaus.effectiveEpiFamily_tfae _ _).out 0 2).2 (fun b => _)
   exact (((ExtrDisc.effectiveEpiFamily_tfae _ _).out 0 2).1 H : ∀ _, ∃ _, _) _
 
@@ -45,7 +45,22 @@ instance : Precoherent ExtrDisc.{u} := by
   intro B₁ B₂ f α _ X₁ π₁ h₁
   refine' ⟨α, inferInstance, fun a => (CompHaus.pullback f (π₁ a)).presentation, fun a => 
     ExtrDisc.toCompHaus.preimage (CompHaus.presentationπ (CompHaus.pullback f (π₁ a)) ≫ (CompHaus.pullback.fst _ _) :
-      toCompHaus.obj (CompHaus.presentation (CompHaus.pullback f (π₁ a))) ⟶ toCompHaus.obj B₂), sorry, sorry⟩
+      toCompHaus.obj (CompHaus.presentation (CompHaus.pullback f (π₁ a))) ⟶ toCompHaus.obj B₂), _, _⟩
+  · refine' ((effectiveEpiFamily_tfae _ _).out 0 2).2 (fun b => _)
+    have h₁' := effectiveEpiFamily.toCompHaus h₁
+    have := (CompHaus.effectiveEpiFamily_tfae _ (Family.toCompHaus' π₁)).out 0 2 ; rw [this] at h₁' ; clear this
+    obtain ⟨a,x,h⟩ := h₁' (f b)
+    set foo : CompHaus.pullback f (π₁ a) := ⟨⟨b, x⟩, h.symm⟩ with foodef
+    obtain ⟨bar, hbar⟩ := (CompHaus.epi_iff_surjective _).1 (CompHaus.epiPresentπ (CompHaus.pullback f (π₁ a))) foo
+    refine' ⟨a, bar, _⟩
+
+    change ExtrDisc.toCompHaus.map (Full.preimage (_ : ExtrDisc.toCompHaus.obj _ ⟶ ExtrDisc.toCompHaus.obj _)) _ = _
+    
+
+    
+
+
+
 
 
 
