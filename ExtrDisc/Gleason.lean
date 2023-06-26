ab4f04d536d103ba8c3ec006b238d5206c5c12ae
faenuccio <filippo.nuccio@univ-st-etienne.fr>
2023-06-26 14:11:54 +0200

    fixed a stupid mistake by filippo

--- ExtrDisc/Gleason.lean
+++ ExtrDisc/Gleason.lean
@@ -1,12 +1,13 @@
 import ExtrDisc.Basic
+import Mathlib.Topology.Constructions
 
-open CategoryTheory
+open CategoryTheory TopologicalSpace
 
 lemma gleason (X : ExtrDisc) : Projective X.compHaus := sorry
 
 variable (A : Type _) [TopologicalSpace A] [T2Space A] [CompactSpace A] [ExtremallyDisconnected A]
-variable [TopologicalSpace B] [TopologicalSpace C] [T2Space B] [T2Space C] [CompactSpace B]
-  [CompactSpace C] 
+variable {B C : Type _} [TopologicalSpace B] [TopologicalSpace C] [T2Space B] [T2Space C]
+  [CompactSpace B] [CompactSpace C] 
 variable {f : B → C} {φ : A → C} (hf : Continuous f) (hφ : Continuous φ) (hφ' : φ.Surjective)
 
 variable (B)
@@ -17,7 +18,5 @@ def π₁ : D A B → A := fun x ↦ x.val.fst
 def π₂ : D A B → B := fun x ↦ x.val.snd
 
 variable {B}
-
-example : TopologicalSpace (D A B) := inferInstance
 
 lemma one : CompactSpace (D A B) := sorry
