f5b47d314d60114b716b7c99ae303537b186412b
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-26 13:41:12 +0200

    start

--- ExtrDisc/Coherent.lean
+++ ExtrDisc/Coherent.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Adam Topaz
 -/
 import ExtrDisc.Basic
+import ExtrDisc.Epi
 
 /-!
 # The category of extremally disconnected spaces is precoherent
@@ -24,6 +25,20 @@ open CategoryTheory
 
 namespace ExtrDisc
 
-instance : Precoherent ExtrDisc.{u} := sorry
+instance : Precoherent ExtrDisc.{u} := by
+  constructor
+  intro B₁ B₂ f α _ X₁ π₁ h₁
+  refine ⟨α, inferInstance, fun a => pullback f (π₁ a), fun a => pullback.fst _ _, ?_,
+    id, fun a => pullback.snd _ _, ?_⟩
+  · have := (effectiveEpiFamily_tfae _ π₁).out 0 2 ; rw [this] at h₁ ; clear this
+    have := (effectiveEpiFamily_tfae _ (fun a => pullback.fst f (π₁ a))).out 0 2
+    rw [this] ; clear this
+    intro b₂
+    obtain ⟨a,x,h⟩ := h₁ (f b₂)
+    refine ⟨a, ⟨⟨b₂, x⟩, h.symm⟩, rfl⟩
+  · intro a
+    dsimp
+    ext ⟨⟨_,_⟩,h⟩
+    exact h.symm
 
 end ExtrDisc
