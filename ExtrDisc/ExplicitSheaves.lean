4e3d2cfd769150f64fd419f640e005164f13b503
dagurtomas <dagurtomas@gmail.com>
2023-06-27 18:14:07 +0200

    Revert "Revert "wip""
    This reverts commit 55e4a54bf354c582903e79bb95b58822daaf695c.

--- ExtrDisc/ExplicitSheaves.lean
+++ ExtrDisc/ExplicitSheaves.lean
@@ -9,5 +9,9 @@ lemma two (F : DCoverage C) : F.toCoverage.toDCoverage = F := sorry
 
 lemma three (F : Coverage C) : F.toGrothendieck = F.toDCoverage.toCoverage.toGrothendieck := sorry
 
+lemma dagur115_vi_to_sheaf (A : Type _) [Category A] [Limits.HasFiniteProducts C]
+  {X : ExtrDisc} (F : ExtrDiscᵒᵖ ⥤ A) (S : Sieve ExtrDisc) (Y : ExtrDisc)
+  (hS : ∀ s s' : S.arrows, Limits.HasPullback s s') : true := sorry
+
 lemma final (A : Type _) [Category A] [Limits.HasFiniteProducts C] (F : ExtrDiscᵒᵖ ⥤ A)
   (hf : Limits.PreservesFiniteProducts F) : Presheaf.IsSheaf (coherentTopology ExtrDisc) F := sorry
