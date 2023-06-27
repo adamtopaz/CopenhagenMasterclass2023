91034773dcffd3114f76e5f519fe92f87b23d0e3
dagurtomas <dagurtomas@gmail.com>
2023-06-27 22:14:30 +0200

    make hS in dagur115_vi_to_sheaf error-free

--- ExtrDisc/ExplicitSheaves.lean
+++ ExtrDisc/ExplicitSheaves.lean
@@ -25,8 +25,9 @@ lemma two (F : DCoverage C) : F.toCoverage.toDCoverage = F := sorry
 lemma three (F : Coverage C) : F.toGrothendieck = F.toDCoverage.toCoverage.toGrothendieck := sorry
 
 lemma dagur115_vi_to_sheaf (A : Type _) [Category A] [Limits.HasFiniteProducts C]
-  {X : ExtrDisc} (F : ExtrDiscᵒᵖ ⥤ A) (S : Sieve ExtrDisc) (Y : ExtrDisc)
-  (hS : ∀ s s' : S.arrows, Limits.HasPullback s s') : true := sorry
+  {X : ExtrDisc} (F : ExtrDiscᵒᵖ ⥤ A) (S : Sieve X)
+  (hS : ∀ (Y Z : ExtrDisc) (s : Y ⟶ X) (s' : Z ⟶ X), 
+  S.arrows s → S.arrows s' → Limits.HasPullback s s') : true := sorry
 
 lemma final (A : Type _) [Category A] [Limits.HasFiniteProducts C] (F : ExtrDiscᵒᵖ ⥤ A)
   (hf : Limits.PreservesFiniteProducts F) : Presheaf.IsSheaf (coherentTopology ExtrDisc) F := sorry
