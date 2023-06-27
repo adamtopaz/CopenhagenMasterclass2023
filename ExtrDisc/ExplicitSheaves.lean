6525486d2214460e398e5eeef727b053bce15af4
dagurtomas <dagurtomas@gmail.com>
2023-06-27 23:04:27 +0200

    add types of maps for statement of vi_to_sheaf

--- ExtrDisc/ExplicitSheaves.lean
+++ ExtrDisc/ExplicitSheaves.lean
@@ -3,15 +3,16 @@ import Sieves.dagur
 import Mathlib.CategoryTheory.Sites.Sheaf
 import ExtrDisc.Coherent
 
+universe u
 
-open CategoryTheory ExtrDisc
+open CategoryTheory ExtrDisc Opposite CategoryTheory.Limits
 
 variable (C : Type _) [Category C] [Precoherent C]
 
-def dagurCoverage [Limits.HasFiniteCoproducts C] : Coverage C where
+def dagurCoverage [HasFiniteCoproducts C] : Coverage C where
   covering B := 
     { S | ∃ (α : Type) (_ : Fintype α) (X : α → C) (π : (a : α) → (X a ⟶ B)),
-    S = Presieve.ofArrows X π ∧ IsIso (Limits.Sigma.desc π) } ∪
+    S = Presieve.ofArrows X π ∧ IsIso (Sigma.desc π) } ∪
     { S | ∃ (X : C) (f : X ⟶ B), S = Presieve.ofArrows (fun (_ : Unit) ↦ X) 
       (fun (_ : Unit) ↦ f) ∧ Epi f }
   pullback := by sorry
@@ -24,9 +25,37 @@ lemma two (F : DCoverage C) : F.toCoverage.toDCoverage = F := sorry
 
 lemma three (F : Coverage C) : F.toGrothendieck = F.toDCoverage.toCoverage.toGrothendieck := sorry
 
+def e {X : ExtrDisc} (F : ExtrDisc.{u}ᵒᵖ ⥤ Type (u+1)) (S : Presieve X) : 
+    F.obj (op X) ⟶ (∀ {Y : ExtrDisc} {s : Y ⟶ X} (hs : S s), F.obj (op Y)) := sorry
+
+def p₁ {X : ExtrDisc} (F : ExtrDisc.{u}ᵒᵖ ⥤ Type (u+1)) (S : Presieve X)
+    (hS : ∀ {Y : ExtrDisc} (s : Y ⟶ X) {Z : ExtrDisc} (s' : Z ⟶ X), 
+    S s → S s' → HasPullback s s') : 
+    (∀ {Y : ExtrDisc} {s : Y ⟶ X} (hs : S s), F.obj (op Y)) ⟶ 
+    (∀ {Y : ExtrDisc} {s : Y ⟶ X} (hs : S s) {Z : ExtrDisc} {s' : Z ⟶ X} (hs' : S s'), 
+    F.obj (op (@Limits.pullback _ _ _ _ _ s s' (hS s s' hs hs')))) := sorry
+
+def p₂ {X : ExtrDisc} (F : ExtrDisc.{u}ᵒᵖ ⥤ Type (u+1)) (S : Presieve X)
+    (hS : ∀ {Y : ExtrDisc} (s : Y ⟶ X) {Z : ExtrDisc} (s' : Z ⟶ X), 
+    S s → S s' → HasPullback s s') : 
+    (∀ {Y : ExtrDisc} {s : Y ⟶ X} (hs : S s), F.obj (op Y)) ⟶ 
+    (∀ {Y : ExtrDisc} {s : Y ⟶ X} (hs : S s) {Z : ExtrDisc} {s' : Z ⟶ X} (hs' : S s'), 
+    F.obj (op (@Limits.pullback _ _ _ _ _ s s' (hS s s' hs hs')))) := sorry
+
+def e_p₁_p₂_Fork {X : ExtrDisc} (F : ExtrDisc.{u}ᵒᵖ ⥤ Type (u+1)) (S : Presieve X)
+    (hS : ∀ {Y : ExtrDisc} (s : Y ⟶ X) {Z : ExtrDisc} (s' : Z ⟶ X), 
+    S s → S s' → HasPullback s s') : Fork (p₁ F S hS) (p₂ F S hS) where
+  pt := F.obj (op X)
+  π := sorry 
+
+def IsEqualizerDiagram_vi_to_sheaf {X : ExtrDisc} (F : ExtrDisc.{u}ᵒᵖ ⥤ Type (u+1)) 
+    (S : Presieve X) (hS : ∀ {Y : ExtrDisc} (s : Y ⟶ X) {Z : ExtrDisc} (s' : Z ⟶ X), 
+    S s → S s' → HasPullback s s') : Prop := sorry --IsLimit (e_p₁_p₂_Fork F S hS)
+
 lemma dagur115_vi_to_sheaf {X : ExtrDisc} (F : ExtrDiscᵒᵖ ⥤ Type _) (S : Presieve X)
     (hS : ∀ {Y : ExtrDisc} (s : Y ⟶ X) {Z : ExtrDisc} (s' : Z ⟶ X), 
-    S s → S s' → Limits.HasPullback s s') : sorry ↔ S.IsSheafFor F := sorry
+    S s → S s' → HasPullback s s') : 
+    IsEqualizerDiagram_vi_to_sheaf F S hS ↔ S.IsSheafFor F := sorry
 
-lemma final (A : Type _) [Category A] [Limits.HasFiniteProducts C] (F : ExtrDiscᵒᵖ ⥤ A)
-  (hf : Limits.PreservesFiniteProducts F) : Presheaf.IsSheaf (coherentTopology ExtrDisc) F := sorry
+lemma final (A : Type _) [Category A] [HasFiniteProducts C] (F : ExtrDiscᵒᵖ ⥤ A)
+  (hf : PreservesFiniteProducts F) : Presheaf.IsSheaf (coherentTopology ExtrDisc) F := sorry
