784de1b84d9f67d3f27a00c8ea4f73e4e7f45b23
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-27 15:07:15 +0200

    this should be good now

--- Profinite/Coherent.lean
+++ Profinite/Coherent.lean
@@ -22,35 +22,6 @@ open CategoryTheory
 
 universe u
 
-variable {X Y B : Profinite.{u}} (f : X ⟶ B) (g : Y ⟶ B)
-
-/--
-The pullback of two morphisms `f,g` in `Profinite`, constructed explicitly as the set of
-pairs `(x,y)` such that `f x = g y`, with the topology induced by the product.
--/
-def pullback : Profinite.{u} :=
-  letI set := { xy : X × Y | f xy.fst = g xy.snd }
-  haveI : CompactSpace set := by
-    rw [← isCompact_iff_compactSpace]
-    apply IsClosed.isCompact
-    exact isClosed_eq (f.continuous.comp continuous_fst) (g.continuous.comp continuous_snd)
-  Profinite.of set
-
-/--
-The projection from the pullback to the first component.
--/
-def pullback.fst : pullback f g ⟶ X where
-  toFun := fun ⟨⟨x,_⟩,_⟩ => x
-  continuous_toFun := Continuous.comp continuous_fst continuous_subtype_val
-
-/--
-The projection from the pullback to the second component.
--/
-def pullback.snd : pullback f g ⟶ Y where
-  toFun := fun ⟨⟨_,y⟩,_⟩ => y
-  continuous_toFun := Continuous.comp continuous_snd continuous_subtype_val
-
-
 instance : Precoherent Profinite.{u} := by
   constructor
   intro B₁ B₂ f α _ X₁ π₁ h₁
