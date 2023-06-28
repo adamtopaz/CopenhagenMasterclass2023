dd6506a1e240db52044ce05864219d354bc5ae0e
Riccardo Brasca <riccardo.brasca@gmail.com>
2023-06-28 14:57:16 +0200

    add sheaf_condition'

--- Sieves/isSheafForPullbackSieve.lean
+++ Sieves/isSheafForPullbackSieve.lean
@@ -1,7 +1,6 @@
-import Mathlib.CategoryTheory.Sites.Pretopology
-import Mathlib.CategoryTheory.Limits.Shapes.Types
+import Mathlib.CategoryTheory.Sites.SheafOfTypes
 
-universe w v₁ v₂ u₁ u₂
+universe v₁ u₁
 
 namespace CategoryTheory
 
@@ -9,12 +8,54 @@ open Opposite CategoryTheory Category Limits Sieve
 
 variable {C : Type u₁} [Category.{v₁} C]
 
-variable {X : C} (R : Presieve X)
+variable {X : C} (S : Presieve X) 
 
 def isPullbackPresieve : Prop :=
-  ∀ Y Z (f : Y ⟶ X) (_ : R f) (g : Z ⟶ X) (_ : R g),
-  -- ∀ fg : (ΣY, { f : Y ⟶ X // R f }) × ΣZ, { g : Z ⟶ X // R g },
+  ∀ {Y Z} {f : Y ⟶ X} (_ : S f) {g : Z ⟶ X} (_ : S g),
   HasPullback f g 
 
+variable (P : Cᵒᵖ ⥤ Type max v₁ u₁)
+
+namespace Equalizer
+
+namespace Presieve
+
+variable (hS : isPullbackPresieve S) {S}
+
+/-- The rightmost object of the fork diagram of https://stacks.math.columbia.edu/tag/00VM, which
+contains the data used to check a family of elements for a presieve is compatible.
+-/
+@[simp] def SecondObj' : Type max v₁ u₁ :=
+  ∏ fun fg : (ΣY, { f : Y ⟶ X // S f }) × ΣZ, { g : Z ⟶ X // S g } =>
+    have := hS fg.1.2.2 fg.2.2.2
+    P.obj (op (pullback fg.1.2.1 fg.2.2.1))
+
+/-- The map `pr₀*` of <https://stacks.math.columbia.edu/tag/00VL>. -/
+noncomputable
+def firstMap' : FirstObj P S ⟶ SecondObj' P hS :=
+    Pi.lift fun fg =>
+    have := hS fg.1.2.2 fg.2.2.2
+    Pi.π _ _ ≫ P.map pullback.fst.op
+
+/-- The map `pr₁*` of <https://stacks.math.columbia.edu/tag/00VL>. -/
+noncomputable def secondMap' : FirstObj P S ⟶ SecondObj' P hS :=
+  Pi.lift fun fg =>
+    have := hS fg.1.2.2 fg.2.2.2
+    Pi.π _ _ ≫ P.map pullback.snd.op
+
+theorem w' : forkMap P S ≫ firstMap' P hS = forkMap P S ≫ secondMap' P hS := by
+  dsimp
+  ext fg
+  simp only [firstMap', secondMap', forkMap]
+  simp only [limit.lift_π, limit.lift_π_assoc, assoc, Fan.mk_π_app]
+  have := hS fg.1.2.2 fg.2.2.2
+  rw [← P.map_comp, ← op_comp, pullback.condition]
+  simp
+
+theorem sheaf_condition' : Nonempty (IsLimit (Fork.ofι _ (w' P hS))) → S.IsSheafFor P := sorry
+
+end Presieve
+
+end Equalizer
 
 end CategoryTheory
