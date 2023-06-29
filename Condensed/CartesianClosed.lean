7f12cc5292c42d6d7db9fccc16cbf161eebf1241
Alex J. Best <alex.j.best@gmail.com>
2023-06-29 14:13:38 +0200

    studd

--- Condensed/CartesianClosed.lean
+++ Condensed/CartesianClosed.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2023 Adam Topaz. All rights reserved.
+Copyright (c) 2023 Alex J. Best. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Adam Topaz
+Authors: Alex J. Best
 -/
 import Mathlib.Topology.Sheaves.Abelian
 import Mathlib.Algebra.Category.GroupCat.Abelian
@@ -12,6 +12,7 @@ import Mathlib.CategoryTheory.Closed.Types
 import Mathlib.CategoryTheory.Closed.Ideal
 import Mathlib.CategoryTheory.Sites.Limits
 import Mathlib.CategoryTheory.Sites.Sheafification
+import Condensed.ConsumeTypeAnnotations
 
 
 /-!
@@ -29,42 +30,29 @@ open CategoryTheory Limits
 
 
 /--
-The category of condensed abelian groups, defined as sheaves of types over
+The category of condensed sets, defined as sheaves of types over
 `CompHaus` with respect to the coherent Grothendieck topology.
 -/
 abbrev CondensedSet := Condensed.{u} (Type (u + 1))
--- notation "Presheaf " C " , " D => Cᵒᵖ ⥤ D
-
-
-section A
-#check CategoryTheory.instCartesianClosedFunctorTypeTypesCategoryInstHasFiniteProductsFunctorTypeTypesCategory
-end A
 
 open GrothendieckTopology
--- def Hom_pre (X Y : CompHaus.{u}ᵒᵖ ⥤ (Type (u + 1))) :
---   CompHaus.{u}ᵒᵖ ⥤ (Type (u + 1)) where
---     obj := fun Z => X.obj Z ⟶ Y.obj Z
---     map := _
---     map_id := _
---     map_comp := _
--- def Hom (X Y : CondensedSet.{u}) : CondensedSet.{u} := sheafify (coherentTopology CompHaus.{u}) (Hom_pre _)
 
 
-instance : HasFiniteProducts (CompHaus.{u}ᵒᵖ ⥤ Type (u + 1)) := by infer_instance
-instance : Category.{u} (CompHaus.{u}ᵒᵖ) := by infer_instance
-
-def AsSmall_Functor_equiv : (CompHaus.{u}ᵒᵖ ⥤ Type (u+1)) ≌ (AsSmall.{u+1} CompHaus.{u}ᵒᵖ ⥤ Type (u+1)) :=
+-- TODO less
+noncomputable
+def AsSmall_Functor_equiv :
+  (CompHaus.{u}ᵒᵖ ⥤ Type (u+1)) ≌ (AsSmall.{u+1} CompHaus.{u}ᵒᵖ ⥤ Type (u+1)) :=
 Equivalence.congrLeft.{u, u + 1, u + 1, u + 1, u + 1, u + 2}
   AsSmall.equiv.{u, u + 1, u + 1}
 
 noncomputable
-instance : CartesianClosed (AsSmall.{u+1} CompHaus.{u}ᵒᵖ ⥤ Type (u + 1)) :=
-CategoryTheory.instCartesianClosedFunctorTypeTypesCategoryInstHasFiniteProductsFunctorTypeTypesCategory
+example : CartesianClosed (AsSmall.{u+1} CompHaus.{u}ᵒᵖ ⥤ Type (u + 1)) :=
+inferInstance
 
 universe v₁ v₂
 instance {C D : Type v₁} [Category C] [Category D] (e : C ⥤ D) [IsEquivalence e] :
   Reflective e := ⟨⟩
---huh fails second time
+--huh fails second time TODO
 -- instance {C D : Type v₁} [Category C] [Category D] (e : C ⥤ D) [IsEquivalence e] :
 --   Reflective e := ⟨⟩
 instance {C D : Type v₁} [Category C] [Category D] (e : C ⥤ D) [IsEquivalence e] :
@@ -110,7 +98,7 @@ variable {J : GrothendieckTopology C}
 instance : Full (sheafToPresheaf J D) := inferInstance
 instance : Faithful (sheafToPresheaf J D) := inferInstance
 -- Generalize this in mathlib?
-instance Condensed.hasFiniteProductsSheaf : HasFiniteProducts (Sheaf J D) where
+instance hasFiniteProductsSheaf : HasFiniteProducts (Sheaf J D) where
   out j := { has_limit := fun F => by infer_instance }
 variable [ConcreteCategory D] [PreservesLimits (forget D)]
   [∀ (P : Cᵒᵖ ⥤ D) (X : C) (S : Cover J X), HasMultiequalizer (Cover.index S P)]
@@ -130,23 +118,17 @@ namespace CondensedSet
 
 noncomputable instance HasFiniteProducts :
     HasFiniteProducts CondensedSet.{u} :=
-Condensed.hasFiniteProductsSheaf -- TODO generalize
+hasFiniteProductsSheaf -- TODO generalize
 
 
-lemma foo :autoParam a s = a:=by rfl
--- noncomputable instance :
--- PreservesLimitsOfShape (Discrete WalkingPair)
---     (leftAdjoint (sheafToPresheaf (coherentTopology CompHaus) (Type (u + 1)))) :=
--- by
---   constructor -- TODO output weird
 noncomputable instance :
-PreservesLimitsOfShape (Discrete WalkingPair)
+  PreservesLimitsOfShape (Discrete WalkingPair)
     (leftAdjoint (sheafToPresheaf (coherentTopology CompHaus) (Type (u + 1)))) :=
-⟨by
+by
+  constructor
   intros K
   show PreservesLimit K (presheafToSheaf _ _)
-  exact PreservesLimitsOfShape.preservesLimit⟩
-  -- convert_to {K : _} → PreservesLimit K (leftAdjoint (sheafToPresheaf (coherentTopology CompHaus) (Type (u + 1))))
+  infer_instance
 
 
 -- noncomputable instance :
