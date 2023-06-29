4ace0019893152514dab92def936539637c54271
sinhp <sinahazratpour@gmail.com>
2023-06-29 14:01:52 +0200

    init

---
+++ Condensed/ClosedSymmMon.lean
@@ -0,0 +1,16 @@
+import Mathlib.Algebra.Category.ModuleCat.Monoidal.Basic
+
+
+/- 
+The category of condensed Abelian groups admits a closed symmetric monoidal structure inherited from the closed symmetric monoidal structure on the category of Abelian groups. 
+
+We shall use Day's reflection principle: 
+https://ncatlab.org/nlab/show/Day%27s+reflection+theorem
+-/
+
+
+/-
+We don't have the closed symmetric monoidal structure on the category of Abelian groups, but only on R-modules for any ring R. 
+-/
+
+
